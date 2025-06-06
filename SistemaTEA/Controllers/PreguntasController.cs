49510ca28c73170147863728838e6ee03ee168d8
Katherine Gutierrez <katherine.gutierrez@catolica.edu.sv>
2025-06-06 03:07:11 -0600

    Subiendo cambios de vistas y  la creacion de MCHAT

---
+++ SistemaTEA/Controllers/PreguntasController.cs
@@ -0,0 +1,151 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SistemaTEA.Models;
+
+namespace SistemaTEA.Controllers
+{
+    public class PreguntasController : Controller
+    {
+        private readonly EvaluacionContext _context;
+        public PreguntasController(EvaluacionContext context)
+        {
+            _context = context;
+        }
+        // GET: PreguntasController
+        public ActionResult Index()
+        {
+            var tipoTest = _context.TiposTest.ToList();
+
+            ViewBag.Preguntas = _context.PreguntasMCHAT.Count();
+            return View(tipoTest);
+        }
+
+        // GET: PreguntasController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: PreguntasController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PreguntasController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult VerPreguntas_MCHAT()
+        {
+            var preguntas = _context.PreguntasMCHAT.ToList();
+            return View(preguntas);
+        }
+
+        //Crear controlador de MCHAT
+        public ActionResult CreateMCHAT()
+        {
+            return View();
+        }
+
+        // POST: PreguntasController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateMCHAT([Bind("PreguntaID,NumeroPregunta, TextoPregunta, EsActiva")] PreguntaMCHAT pregunta_M, int id_test)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    pregunta_M.NumeroPregunta = _context.PreguntasMCHAT.Count()  ; // Asignar número de pregunta
+                    _context.PreguntasMCHAT.Add(pregunta_M);
+                    _context.SaveChanges();
+
+                    return RedirectToAction("CreateMCHAT", "Preguntas", new { id = id_test });
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        public ActionResult EditMCHAT()
+        {
+            var preguntas = _context.PreguntasMCHAT.ToList();
+            return View(preguntas);
+        }
+
+        // POST: PreguntasController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditMCHAT(int id_pregunta, IFormCollection collection)
+        {
+            try
+            {
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        // GET: PreguntasController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: PreguntasController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PreguntasController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PreguntasController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
